f2954a14d33373f8a260a5da084700eb808bd26e
ymdzq <233900743@qq.com>
2022-05-06 20:47:14 +0800

    参照官方recovery目录添加三个空文件夹

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,6 +89,7 @@ BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
 
 # System as root
+BOARD_ROOT_EXTRA_FOLDERS := cache cust product
 BOARD_SUPPRESS_SECURE_ERASE := true
 
 # Recovery
