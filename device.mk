3ce97593c7d17123b0a2b2805d22774232fcf843
ymdzq <233900743@qq.com>
2022-05-14 22:01:44 +0800

    加入Fastbootd

--- device.mk
+++ device.mk
@@ -14,6 +14,12 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_TARGET_VNDK_VERSION := 31
 PRODUCT_SHIPPING_API_LEVEL := 29
 
+# Fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery \
+    fastbootd
+
 # Vibrator
 PRODUCT_PACKAGES += \
 	android.hardware.vibrator@1.3-service.mtk.recovery
