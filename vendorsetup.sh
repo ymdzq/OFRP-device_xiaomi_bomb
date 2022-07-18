310e8fcabeec495810c30f78490ae15a07857ef8
ymdzq <233900743@qq.com>
2022-07-18 21:20:28 +0800

    调整橙狐变量顺序 无实际意义，纯粹为了方便看

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -37,18 +37,6 @@
     # 添加phhusson's lptools工具
     export OF_ENABLE_LPTOOLS=1
 
-    ## 添加橙狐特殊处理
-    # 当安装MIUI或者在MIUI上安装橙狐zip，使用magisk处理所有boot和recovery镜像，防止橙狐被MIUI官方recovery替换
-    export OF_FORCE_MAGISKBOOT_BOOT_PATCH_MIUI=1
-    # 防止用户在不使用metadata加密的ROM中重复被metadata装载错误提示刷屏
-    export OF_FBE_METADATA_MOUNT_IGNORE=1
-    # 尝试处理AVB2.0，防止橙狐被官方recovery替换，实测此机型无效
-    # export OF_PATCH_AVB20=1
-    # 修复部分设备或者rom的解密问题
-    export OF_FIX_DECRYPTION_ON_DATA_MEDIA=1
-    # 当修补recovery/boot镜像时，始终指示magiskboot v24+修补vbmeta标头（自动禁用avb验证？）
-    export OF_PATCH_VBMETA_FLAG=1
-
     ## 硬件功能设定
     # 没有绿色led
     export OF_USE_GREEN_LED=0
@@ -80,13 +68,15 @@
     # 使橙狐可以刷入具有机型检测限制为红米10X 5G或者红米10X Pro的zip卡刷包，与TARGET_OTA_ASSERT_DEVICE冲突
     # export OF_TARGET_DEVICES="atom,bomb"
 
-    ## 技术性调整
+    ## 添加橙狐特殊处理
     # 设定recovery、system、vendor、boot分区路径
     export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/by-name/recovery"
     export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
     export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
     export FOX_RECOVERY_BOOT_PARTITION="/dev/block/by-name/boot"
 
+    # 当安装MIUI或者在MIUI上安装橙狐zip，使用magisk处理所有boot和recovery镜像，防止橙狐被MIUI官方recovery替换
+    export OF_FORCE_MAGISKBOOT_BOOT_PATCH_MIUI=1
     # 避免在已加密设备上应用强制加密补丁
     export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
     # 跳过FBE解密流程（防止卡在橙狐LOGO或Redmi/Mi LOGO界面）
@@ -97,11 +87,19 @@
     export OF_OTA_RES_DECRYPT=1
     # 防止橙狐在解密后重新运行自启动进程
     export OF_NO_RELOAD_AFTER_DECRYPTION=1
+    # 防止用户在不使用metadata加密的ROM中重复被metadata装载错误提示刷屏
+    export OF_FBE_METADATA_MOUNT_IGNORE=1
+    # 尝试处理AVB2.0，防止橙狐被官方recovery替换，实测此机型无效
+    # export OF_PATCH_AVB20=1
 	# 关闭修改橙狐启动画面功能，这个功能可能会导致重启无限卡MI
     export OF_NO_SPLASH_CHANGE=1
     # 使用指定的magisk
     export FOX_USE_SPECIFIC_MAGISK_ZIP="$HOME/Magisk.zip"
     # 使用指定的magisk版本号，由于magisk 23+使用了新的包装形式，文件路径改变了，橙狐无法获取正确的版本
     export MAGISK_VER=25.1
+    # 当修补recovery/boot镜像时，始终指示magiskboot v24+修补vbmeta标头（自动禁用avb验证？）
+    export OF_PATCH_VBMETA_FLAG=1
+    # 修复部分设备或者rom的解密问题
+    export OF_FIX_DECRYPTION_ON_DATA_MEDIA=1
 
     echo -e "\x1b[96mbomb: 当你看到这个消息的时候，所有的OrangeFox Var已经添加完毕！\x1b[m"
